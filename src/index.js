08cd3654d9d57c3ee81cefd77a111f81722fc10d
Jeisson Gómez <jeissongomez@hotmail.es>
2022-05-02 11:14:48 -0500

    Import, export y funciones comunes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-const tarea = (valor) => {
-  return [
-    valor,
-    () => {
-      console.log("Hola Mundo");
-    },
-  ];
-};
-const [nombre, logNombre] = tarea("Goku");
-console.log(nombre);
-logNombre();
+
